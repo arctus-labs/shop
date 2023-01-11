cc8da6a3e03174c9af1e817d138315ebbcec5476
nsde <neostyxde@gmail.com>
2023-01-11 21:18:36 +0100

    Added navbar scrolling effect

--- arctus/static/js/nav.js
+++ arctus/static/js/nav.js
@@ -1,24 +1,28 @@
 function openMobileNav() {
-    const nav = document.getElementById('mobnav');
+    const navPopup = document.getElementById('mobnav');
     const navIcon = document.getElementById('mobnav-icon');
 
-    nav.classList.toggle('foldout');
+    navPopup.classList.toggle('foldout');
     navIcon.classList.toggle('open');
     
-    if (nav.classList.contains('foldout')) {
-        nav.style.height = '100vh';
+    if (navPopup.classList.contains('foldout')) {
+        navPopup.style.height = '100vh';
     } else {
-        nav.style.height = '0';
+        navPopup.style.height = '0';
     }
 }
 
-// window.onscroll = function() {
-//     const nav = document.getElementById('mobnav');
-//     var currentScrollPos = window.pageYOffset;
-  
-//     if (currentScrollPos > 20) {
-//         nav.style.transform = "translateY(-100%)";
-//     } else {
-//         nav.style.display = "initial";
-//     }
-//   }
+var lastScrollTop;
+
+window.onscroll = function() {
+    var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    navbar = document.getElementById('header');
+
+    if(scrollTop > lastScrollTop) {
+        navbar.style.top = '-90px';
+    } else{
+        navbar.style.top='0';
+    }
+
+    lastScrollTop = scrollTop;
+};
